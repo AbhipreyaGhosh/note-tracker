3548af10c7b6729c2bd0baf1c034837e9ce43a5a
AbhipreyaGhosh <ghoshabhipreya@gmail.com>
2025-05-14 22:09:59 +0530

    commiting on this branch

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -258,6 +258,10 @@
 //   },
 // });
 
+
+// image store
+
+
 import * as FileSystem from "expo-file-system";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
